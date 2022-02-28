e2b8659a09039b13d3e30a3e8faa413214d12008
StellarisJAY <760409558@qq.com>
2022-02-28 16:19:15 +0800

    添加Prometheus HTTP服务器

---
+++ src/main/java/com/jay/rpc/prometheus/PrometheusServer.java
@@ -0,0 +1,61 @@
+package com.jay.rpc.prometheus;
+
+import com.jay.dove.common.AbstractLifeCycle;
+import com.jay.rpc.config.MiniRpcConfigs;
+import io.prometheus.client.Collector;
+import io.prometheus.client.exporter.HTTPServer;
+import io.prometheus.client.hotspot.DefaultExports;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * <p>
+ *  Prometheus监控HTTP服务器
+ * </p>
+ *
+ * @author Jay
+ * @date 2022/02/28 15:42
+ */
+@Slf4j
+public class PrometheusServer extends AbstractLifeCycle {
+
+    private HTTPServer httpServer;
+    private final List<Collector> collectors = new LinkedList<>();
+
+    /**
+     * 添加Collector
+     * @param collector {@link Collector}
+     */
+    public void addCollector(Collector collector){
+        collectors.add(collector);
+    }
+
+    private void init() throws Exception{
+        // 注册collector
+        for (Collector collector : collectors) {
+            collector.register();
+        }
+        // 初始化JVM信息Exporter
+        DefaultExports.initialize();
+        // 启动Prometheus HTTP服务器
+        this.httpServer = new HTTPServer(MiniRpcConfigs.prometheusServerPort(), true);
+    }
+
+    @Override
+    public void startup() {
+        super.startup();
+        try{
+            init();
+        }catch (Exception e){
+            log.error("prometheus server error ", e);
+        }
+    }
+
+    @Override
+    public void shutdown() {
+        super.shutdown();
+        httpServer.stop();
+    }
+}
