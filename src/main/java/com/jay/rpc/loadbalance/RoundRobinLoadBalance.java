34100bf79f31aaff63cef6eca0f2b2e0cff47659
StellarisJAY <760409558@qq.com>
2022-02-28 12:29:59 +0800

    添加轮询负载均衡

---
+++ src/main/java/com/jay/rpc/loadbalance/RoundRobinLoadBalance.java
@@ -0,0 +1,20 @@
+package com.jay.rpc.loadbalance;
+
+import com.jay.rpc.registry.ProviderNode;
+
+import java.util.List;
+
+/**
+ * <p>
+ *  轮询负载均衡
+ * </p>
+ *
+ * @author Jay
+ * @date 2022/02/28 12:26
+ */
+public class RoundRobinLoadBalance extends AbstractLoadBalance{
+    @Override
+    public ProviderNode doSelect(List<ProviderNode> providerNodes) {
+        return null;
+    }
+}
