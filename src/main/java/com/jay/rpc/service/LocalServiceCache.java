153416685668e62bb997d3b676de6b720e268100
StellarisJAY <760409558@qq.com>
2022-04-06 13:06:24 +0800

    添加服务提供者端的服务实例缓存

---
+++ src/main/java/com/jay/rpc/service/LocalServiceCache.java
@@ -0,0 +1,33 @@
+package com.jay.rpc.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author Jay
+ * @date 2022/03/22 13:35
+ */
+public class LocalServiceCache {
+    private static final ConcurrentHashMap<ServiceInfo, ServiceInstance> INSTANCES = new ConcurrentHashMap<>();
+
+    public static ServiceInstance getServiceInstance(ServiceInfo serviceInfo){
+        return INSTANCES.get(serviceInfo);
+    }
+
+    public static void registerServiceInstance(ServiceInfo serviceInfo, ServiceInstance instance) throws Exception {
+        if(INSTANCES.get(serviceInfo) != null){
+            throw new Exception("register service failed, duplicate service info");
+        }
+        INSTANCES.put(serviceInfo, instance);
+    }
+
+    public static List<ServiceInfo> listServices(){
+        return new ArrayList<>(INSTANCES.keySet());
+    }
+}
